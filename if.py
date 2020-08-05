7f5cef886ffb82600d8e35470fc3d63836d99bc2
Hanaho Murata <nyaaaaaan0129@gmail.com>
2020-08-05 19:21:11 +0900

    if

---
+++ if.py
@@ -0,0 +1,10 @@
+your_point = 92
+your_name = 'Takanori Suzuki'
+if your_point >= 80:
+	print('Excellent!')
+
+if your_point >= 80 and your_name == 'Takanori Suzuki':
+    print('あなたは点数が高い方の鈴木さんです')
+
+if your_name == 'Takanori Suzuki' or your_name == 'Takayuki Shimizukawa':
+	print('あなたはこの本の製作者メンバーです')
