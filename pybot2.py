91c262c998fdbf168c9419fd394d447fb221f258
Hanaho Murata <nyaaaaaan0129@gmail.com>
2020-08-09 17:51:33 +0900

    pybot2

---
+++ pybot2.py
@@ -0,0 +1,25 @@
+command_file = open('pybot.txt', encoding='utf-8')
+raw_data = command_file.read()
+command_file.close()
+lines = raw_data.splitlines()
+
+bot_dict = {}
+for line in lines:
+	word_list = line.split(',')
+	key = word_list[0]
+	response = word_list[1]
+	bot_dict[key] = response
+
+while True:
+    command = input('pybot>')
+    response = ''
+    for message in bot_dict:
+	    if message in command:
+	        response = bot_dict[message]
+	        break
+    if not response:
+    	response = '何ヲ言ッテルカ、ワカラナイ'
+    print(response)
+
+    if 'さようなら' in command:
+    	break
