539cff7f62b0be8d330482a842180feed104d28f
Hanaho Murata <nyaaaaaan0129@gmail.com>
2020-08-09 18:09:25 +0900

    平成

--- pybot2.py
+++ pybot2.py
@@ -17,6 +17,15 @@ while True:
 	    if message in command:
 	        response = bot_dict[message]
 	        break
+    if '平成' in command:
+        heisei, year_str = command.split()
+        year = int(year_str)
+        if year >= 1989:
+            heisei_year = year - 1988
+            response = '西暦{}年ハ、平成{}年デス'.format(year, heisei_year)
+        else:
+            response = '西暦{}年ハ、平成デハアリマセン'.format(year)
+
     if not response:
     	response = '何ヲ言ッテルカ、ワカラナイ'
     print(response)
